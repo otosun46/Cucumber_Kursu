71ccc8ce5ccfe71fd3439f4ad9c709c274713903
Otosun <64793578+otosun46@users.noreply.github.com>
2020-10-23 16:57:28 +0200

    Guncelleme

--- src/main/java/ApachePOI/_09_CarpimTablosuOdev.java
+++ src/main/java/ApachePOI/_09_CarpimTablosuOdev.java
@@ -15,24 +15,21 @@ public class _09_CarpimTablosuOdev {
     public static void main(String[] args) throws IOException {
         String path = "src/main/resources/CarpimTablosu.xlsx";
         XSSFWorkbook workbook = new XSSFWorkbook();
-        XSSFSheet sheet = workbook.createSheet("Benim Sayfam");
+        XSSFSheet sheet = workbook.createSheet("Carpim Tablosu");
 
         Row row =null;
         Cell cell =null;
-        int rowCount=0;
         int add=0;
 
         for (int i = 1; i <= 10; i++) {
-            row = sheet.createRow(rowCount++);
-            int cellCarpan=0;
+            row = sheet.createRow(i-1);
             for (int j = 1; j <= 10; j++) {
-                add=cellCarpan*6;
-                cell = row.createCell(0+add); cell.setCellValue(i+"");
+                add=(j-1)*6;
+                cell = row.createCell(0+add); cell.setCellValue(i);
                 cell = row.createCell(1+add); cell.setCellValue(" X ");
-                cell = row.createCell(2+add); cell.setCellValue(j+"");
+                cell = row.createCell(2+add); cell.setCellValue(j);
                 cell = row.createCell(3+add); cell.setCellValue(" = ");
-                cell = row.createCell(4+add); cell.setCellValue(""+i * j);
-                cellCarpan++;
+                cell = row.createCell(4+add); cell.setCellValue(i * j);
             }
         }
         FileOutputStream fileOutputStream = new FileOutputStream(path);
